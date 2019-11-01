d95d9e18acafd53a11785ca2a0c35d1c09c90cac
UshanShakya <usnshakya4@gmail.com>
2019-11-01 18:27:39 +0545

    making a reeport class to store the report value

---
+++ app/src/main/java/Class/Reports.java
@@ -0,0 +1,32 @@
+package Class;
+
+public class Reports {
+    private String userName;
+    private String Location;
+    private String Report;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getReport() {
+        return Report;
+    }
+
+    public void setReport(String report) {
+        Report = report;
+    }
+}
+
